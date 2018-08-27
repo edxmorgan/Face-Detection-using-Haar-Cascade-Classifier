8adb81ec462819eb4a96e4cb89704ff1913c4dd2
blvck <blvck@mars.mshome.net>
2018-08-27 16:27:17 -0600

    complete

--- face_detection/detect_faces.py
+++ face_detection/detect_faces.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from faceDetector import FaceDetector
 import argparse
 import cv2
 
@@ -12,3 +13,13 @@ args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+fd = FaceDetector(args["face"])
+faceRects = fd.detect(gray, scaleFactor=1.1,minNeighbors=5,
+    minSize = (30,30))
+print("I found {} face(s)".format(len(faceRects)))
+
+for (x,y,w,h) in faceRects:
+    cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+
+cv2.imshow("Faces", image)
+cv2.waitKey(0)
